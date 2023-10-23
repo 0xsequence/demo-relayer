30566c4ffab8ec31dcc119d94a3decbb1d23bea8
moskalyk <morgan.moskalyk@protonmail.ch>
2023-10-23 17:34:41 -0400

    added project with error

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# demo-relayer
+# relayer-cli
+
+# functions
+```
+$ ./sequence-demo.ts
+
+var mastHead = "\n _____                             \n|   __|___ ___ _ _ ___ ___ ___ ___ \n|__   | -_| . | | | -_|   |  _| -_|\n|_____|___|_  |___|___|_|_|___|___|\n            |_|\n            \n  Release: v".concat("\n\n");
+
+1. wallet
+2. claim
+3. send
+
+$ ./sequence-demo.ts wallet
+$ ./sequence-demo.ts claim
+green: your tx will cost ~XX gas, would you like to proceed y/n: y
+yellow: minted xx tokens, costing XX gas
+red: operation aborted
+
+$ ./sequence-demo.ts send <amount> to <address>
+```
---
+++ sequence-demo-relayer.ts
@@ -0,0 +1,148 @@
+import fs from 'fs-extra';
+import path from 'path';
+import dotenv from 'dotenv';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import { Session } from '@0xsequence/auth'
+import { Command } from 'commander'
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import { ethers } from 'ethers';
+
+const program = new Command();
+
+async function generateOrLoadPrivateKey() {
+    const envFilePath = path.join(__dirname, '.env');
+
+    // Check if .env file exists
+    if (fs.existsSync(envFilePath)) {
+        dotenv.config();
+
+        // Check if pkey exists in .env
+        if (process.env.pkey) {
+            console.log('Private key already exists in .env file.');
+            return process.env.pkey;
+        }
+    }
+
+    // If not, generate a new wallet and private key
+    const wallet = ethers.Wallet.createRandom();
+    const privateKey = wallet.privateKey;
+
+    // Write private key to .env file
+    fs.appendFileSync(envFilePath, `pkey=${privateKey}\n`);
+    console.log('Generated and saved a new private key to .env file.');
+
+    return privateKey;
+}
+
+program
+    .name('sequence-demo-relayer')
+    .description(chalk.blue('CLI to claim and send ERC20 tokens called $DEMO.\n\n _____                             \n|   __|___ ___ _ _ ___ ___ ___ ___ \n|__   | -_| . | | | -_|   |  _| -_|\n|_____|___|_  |___|___|_|_|___|___|\n            |_|\n '))
+    .version('0.0.1');
+
+program.command('wallet')
+    .description('generate a wallet, if not created locally and print wallet address')
+    .action((str: any, options: any) => {
+        generateOrLoadPrivateKey().then(async (privateKey) => {
+            const providerUrl = 'https://nodes.sequence.app/arbitrum-goerli';
+            const provider = new ethers.providers.JsonRpcProvider(providerUrl);
+
+            // Create your server EOA
+            const walletEOA = new ethers.Wallet(privateKey, provider);
+
+            // Open a Sequence session, this will find or create
+            // a Sequence wallet controlled by your server EOA
+            
+            // this doesn't work
+            const session = await Session.singleSigner({
+                signer: walletEOA,
+                // OPTIONAL: Multiple wallets could be found for the same EOA
+                // to enforce a specific wallet you can use the following callback
+            })
+            
+            // const signer = session.account.getSigner(421613, {
+            //     // OPTIONAL: You can also enforce a specific way to pay for gas fees
+            //     // if not provided the sdk will select one for you
+            //     selectFee: async (
+            //         _txs: any,
+            //         options: any[]
+            //     ) => {
+            //         console.log(options)
+            //         const found = options.find(option => option.token.symbol === 'MATIC')
+            //         if (!found) throw Error('fee option not found')
+            //         return found
+            //     }
+            // })
+            
+            // console.log(signer.account.address)
+            // console.log(chalk.blue(`Your wallet address: ${signer.account.address}`))
+        }).catch(error => {
+            console.error(`Failed to generate or load private key: ${error}`);
+        });
+    });
+
+program.command('claim')
+    .description('claim some $DEMO token from the faucet')
+    .action(async (str: any, options: any) => {
+
+        const providerUrl = 'https://nodes.sequence.app/arbitrum-goerli';
+        const provider = new ethers.providers.JsonRpcProvider(providerUrl);
+
+        const erc721Interface = new ethers.utils.Interface([
+            'function mint()'
+        ])
+            
+        const data = erc721Interface.encodeFunctionData(
+            'mint', []
+        )
+            
+        const txn = {
+            to: "0x4035c18d55a8e78638dF0E7240cD347BfE9F4c1A",
+            data
+        }
+        
+        // console.log(signer.options.selectFee(txn, []))
+        const gasEstimate = await provider.estimateGas(txn);
+        
+        const answers = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'userInput',
+                message: chalk.greenBright(`Your tx will cost ~${gasEstimate.toString()} gas, would you like to proceed y/n`),
+            }
+        ]);
+
+        // After getting user input, continue with the function
+        if(answers.userInput == 'y'){
+            console.log(`Transaction ID: ${null}`)
+        } else {
+            console.log(chalk.red(`User denied tx.`))
+        }
+    });
+
+program.command('send')
+    .description('send a certain number of tokens to a friends address')
+    .argument('<amount>', 'amount to send')
+    .argument('<address>', 'wallet address to send to')
+    // .option('--first', 'display just the first substring')
+    // .option('-s, --separator <char>', 'separator character', ',')
+    .action((amount, address, options) => {
+        console.log(amount)
+        console.log(address)
+        // const limit = options.first ? 1 : undefined;
+        // console.log(str.split(options.separator, limit));
+        console.log(`Transaction ID: ${null}`)
+});
+  
+program.parse();
+
+function myFunction(input: any) {
+    console.log(`You entered: ${input}`);
+    // Your function logic here
+}
